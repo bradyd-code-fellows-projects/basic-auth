2a0511d500c5e6b9610abcf323ba240b315176c2
Brady <brady.davenport@pm.me>
2022-06-20 19:02:21 -0600

    db url, sequelize

--- models/index.js
+++ models/index.js
@@ -1,29 +1,21 @@
 'use strict';
 
-let base64 = require('base-64');
-let bcrypt = require('bcrypt');
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-console.log('==================BASE 64========================');
+const DATABASE_URL = process.env.NODE_ENV === 'test'
+  ? 'sqlite::memory'
+  : process.env.DATABASE_URL || 'sqlite:memory';
 
-let str = 'username:password';
+const sequelize = new Sequelize(DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
-let encoded = base64.encode(str);
-let decoded = base64.decode(encoded);
-
-console.log('str: ', str);
-console.log('encoded: ', encoded);
-console.log('decoded: ', decoded);
-
-console.log('===============HASHING==========================');
-
-let password1 = 'abc123';
-let complexity1 = 5;
-
-async function encrypt(password, complexity) {
-  let hashedPass = await bcrypt.hash(password, complexity);
-  let exampleOne = '$2a$05$CUE2QFUwCArrheh0KuyI.O26NEN4hJZ7egJ3oiRCXaWFli/CQHqPm';
-  let isValidOne = await bcrypt.compare(password, hashedPass);
-  console.log('isValidOne: ', isValidOne);
-}
-
-encrypt(password1, complexity1);
+module.exports = {
+  sequelize,
+};
